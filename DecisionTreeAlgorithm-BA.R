b99494ae47efe975dd891f52cc545796e6dffb72
Arjun <arjunsekar1991@gmail.com>
2019-06-17 20:52:54 -0400

    decision tree final

--- DecisionTreeAlgorithm-BA.R
+++ DecisionTreeAlgorithm-BA.R
@@ -6,10 +6,6 @@ library(rpart)
 library(rattle)
 #install.packages('caret', dependencies = TRUE)
 library(caret)
-#install.packages("rpart.plot")
-
-
-library(rpart.plot)
 
 
 dataframe <- read.csv("data_banknote_authentication.txt", header = FALSE)
@@ -20,14 +16,21 @@ splitData <- sample(2, nrow(dataframe), replace = TRUE, prob = c(0.7,0.3))
 trainingData <- dataframe[splitData==1,]
 testData <- dataframe[splitData==2,]
 
-tree.train = rpart(Class~., data=trainingData)
-fancyRpartPlot(tree.train)
+
+
+trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+set.seed(100)
+tree.train = train(Class~., method = "rpart",
+                   parms = list(split = "information"),
+                   trControl=trctrl,
+                   tuneLength = 10,trainingData)
+fancyRpartPlot(tree.train$finalModel)
 
 #prediction for training
-tree.predict1 <-predict(tree.train, trainingData, type="class") 
+tree.predict1 <-predict(tree.train, trainingData) 
 print(tree.predict1)
 confusionMatrix(tree.predict1, trainingData$Class)
 
 #prediction for testData
-tree.predict2 <- predict(tree.train, testData, type="class")
+tree.predict2 <- predict(tree.train, testData)
 confusionMatrix(tree.predict2, testData$Class)
